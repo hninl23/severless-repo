a2baa09f5887c81dd99da0bab1c3698ff17dc0db
HninL <100245412+hninl23@users.noreply.github.com>
2022-06-10 08:23:03 -0400

    Update late.js

--- late.js
+++ late.js
@@ -1,1 +1,15 @@
 // start coding your function here!
+let hour = new Date ().getHours()
+function running_late()
+{
+if (hour > 22 )
+{
+    return"It is late!"
+}
+else
+{
+    return "It is still early!"
+}
+}
+
+exports.running_laate = running_late;
