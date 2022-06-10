b7736f7d49227618e7db3f31c64ecd81f1fad805
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-10 19:47:52 -0400

    change name

--- late.js
+++ late.js
@@ -1,1 +1,15 @@
 // start coding your function here!
+function running_late()
+let hour = new Date ().getHours()
+{
+if (hour > 22 )
+{
+    return"It is late!"
+}
+else
+{
+    return "It is still early!"
+}
+}
+
+exports.running_laate = running_late;
