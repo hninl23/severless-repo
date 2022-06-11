f305fbec5cbc009a5263cb9b07705385eefb4506
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-11 18:26:43 -0400

    Change Code

--- late.js
+++ late.js
@@ -1,1 +1,12 @@
 // start coding your function here!
+var date = new Date ().getHours();
+function running_late(date){
+    if (date > 22 )
+{
+    return"It is late!";
+}
+    return "It is still early!";
+}
+
+ 
+exports.running_late = running_late;
