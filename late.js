ca885a66fa011153449578e592462b63938562af
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-11 23:17:56 -0400

    update the runninglate function

--- late.js
+++ late.js
@@ -1,12 +1,15 @@
 // start coding your function here!
-var date = new Date ().getHours();
+
+var date = new Date();
 function running_late(date){
-    if (date > 22 )
-{
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    if (h >= 21 && m >= 00 && s>=00)
     return"It is late!";
-}
     return "It is still early!";
 }
 
- 
 exports.running_late = running_late;
