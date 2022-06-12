3fc5f0b676780a09b8663899588d661eb4677ecd
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-12 09:46:30 -0400

    change argument in declaration

--- late.js
+++ late.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+var date = new Date(dateString);
+function running_late(date){
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    if (h >= 21 && m >= 00 && s>=00)
+    return"It is late!";
+    return "It is still early!";
+}
+
+exports.running_late = running_late;
