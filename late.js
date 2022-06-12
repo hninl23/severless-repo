e8ab6e6b55ec139296aaea6b01450d5c785aad8d
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-12 10:27:17 -0400

    change where we decalre the date

--- late.js
+++ late.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+
+function running_late(date){
+    var date = new Date(dateString);
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    if (h >= 21 && m >= 00 && s>=00)
+    return"It is late!";
+    return "It is still early!";
+}
+exports.running_late = running_late; 
