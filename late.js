ff342b5d6d7263d1a4f37058c3d776c2ab5f2eba
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-12 12:41:09 -0400

    change conditions

--- late.js
+++ late.js
@@ -3,18 +3,18 @@
 function running_late(date){
     var date = new Date();
     var h = date.getHours();
-    //var m = date.getMinutes();
-    //var s = date.getSeconds();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
 
-    if (h >= 22) {
-        return"It is late!";
-    }
-        //&& m >= 00 && s>=00)
-    else {
-        return "It is still early!"; 
-    }
+    if (h >= 22){
+        return "It is late!";
+
+    }else {
+        return "It is still early!";   
+        }
         
     }
-          
-
+//var date = new Date("December 17, 1995 23:24:00");
+//console.log(date.getHours("December 17, 1995 22:24:00"))
+//console.log(running_late("December 17, 1995 22:24:00"))
 exports.running_late = running_late; 
